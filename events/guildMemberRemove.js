d80a6ed949b4ee40e29b0d23726f1d8621889b86
rahatsizarmy <37729661+rahatsizarmy@users.noreply.github.com>
2018-03-30 23:36:29 +0300

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,7 +0,0 @@
-module.exports = member => {
-  let guild = member.guild;
-  if (message.channel.type !== 'dm') {
-  member.send('niye gittin?');
-  guild.defaultChannel.send(`${member.user.username} gitti.`);
-  }
-};
